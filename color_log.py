2260b55d9563c9a92eddf776aa97d5cbf748e15f
dhananjay.umalkar <dhananjay.umalkar@pointcloudtechnology.com>
2023-03-31 14:29:32 +0200

    updated color log

--- color_log.py
+++ color_log.py
@@ -1,25 +1,37 @@
 import logging
-from termcolor import colored
+import colorlog
 
-class ColoredFormatter(logging.Formatter):
-    def format(self, record):
-        if record.levelno == logging.ERROR:
-            # Use red color for error messages
-            record.msg = colored(record.msg, 'red')
-        elif record.levelno == logging.WARNING:
-            # Use orange color for warning messages
-            record.msg = colored(record.msg, 'yellow')
-        return super().format(record)
+# create logger
+logger = logging.getLogger(__name__)
 
-# Create a logger
-logger = logging.getLogger('my_logger')
-logger.setLevel(logging.WARNING)
+# set log level
+logger.setLevel(logging.DEBUG)
 
-# Create a console handler with the custom formatter
-handler = logging.StreamHandler()
-handler.setFormatter(ColoredFormatter('%(levelname)s: %(message)s'))
-logger.addHandler(handler)
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
 
-# Log an error message and a warning message
+# create formatter with color scheme
+formatter = colorlog.ColoredFormatter(
+    '%(log_color)s%(levelname)s:%(message)s',
+    log_colors={
+        'DEBUG': 'green',
+        'INFO': 'white',
+        'WARNING': 'yellow',
+        'ERROR': 'red',
+        'CRITICAL': 'red,bg_white',
+    }
+)
+
+# add formatter to console handler
+ch.setFormatter(formatter)
+
+# add console handler to logger
+logger.addHandler(ch)
+
+# log a debug message
+logger.debug('This is a debug message in green color!')
 logger.error('An error occurred!')
 logger.warning('Something might be wrong!')
+logger.critical('A critical error occurred!')
+logger.info('This is an info message in white color!')
