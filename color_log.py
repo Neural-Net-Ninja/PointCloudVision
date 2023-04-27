07eb8ca98c61e4a1635b9dc28ad0dec5146f055a
dhananjay.umalkar <dhananjay.umalkar@pointcloudtechnology.com>
2023-04-27 14:10:59 +0200

    logger updated

--- color_log.py
+++ color_log.py
@@ -1,37 +1,72 @@
 import logging
 import colorlog
 
-# create logger
-logger = logging.getLogger(__name__)
 
-# set log level
-logger.setLevel(logging.DEBUG)
+class logging_config:
 
-# create console handler and set level to debug
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
+    def logging(self):
+        # create logger
+        logger = logging.getLogger(__name__)
 
-# create formatter with color scheme
-formatter = colorlog.ColoredFormatter(
-    '%(log_color)s%(levelname)s:%(message)s',
-    log_colors={
-        'DEBUG': 'green',
-        'INFO': 'white',
-        'WARNING': 'yellow',
-        'ERROR': 'red',
-        'CRITICAL': 'red,bg_white',
-    }
-)
+        # set log level
+        logger.setLevel(logging.DEBUG)
 
-# add formatter to console handler
-ch.setFormatter(formatter)
+        # create console handler and set level to debug
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
 
-# add console handler to logger
-logger.addHandler(ch)
+        # create formatter with color scheme
+        formatter = colorlog.ColoredFormatter(
+            '%(log_color)s%(levelname)s:%(message)s',
+            log_colors={
+                'DEBUG': 'green',
+                'INFO': 'white',
+                'WARNING': 'yellow',
+                'ERROR': 'red',
+                'CRITICAL': 'red,bg_white',
+            }
+        )
 
-# log a debug message
-logger.debug('This is a debug message in green color!')
-logger.error('An error occurred!')
-logger.warning('Something might be wrong!')
-logger.critical('A critical error occurred!')
-logger.info('This is an info message in white color!')
+        # add formatter to console handler
+        ch.setFormatter(formatter)
+
+        # add console handler to logger
+        logger.addHandler(ch)
+
+        return logger
+# # create logger
+# logger = logging.getLogger(__name__)
+
+# # set log level
+# logger.setLevel(logging.DEBUG)
+
+# # create console handler and set level to debug
+# ch = logging.StreamHandler()
+# ch.setLevel(logging.DEBUG)
+
+# # create formatter with color scheme
+# formatter = colorlog.ColoredFormatter(
+#     '%(log_color)s%(levelname)s:%(message)s',
+#     log_colors={
+#         'DEBUG': 'green',
+#         'INFO': 'white',
+#         'WARNING': 'yellow',
+#         'ERROR': 'red',
+#         'CRITICAL': 'red,bg_white',
+#     }
+# )
+
+# # add formatter to console handler
+# ch.setFormatter(formatter)
+
+# # add console handler to logger
+# logger.addHandler(ch)
+
+# # log a debug message
+# logger.debug('This is a debug message in green color!')
+# logger.error('An error occurred!')
+# logger.warning('Something might be wrong!')
+# logger.critical('A critical error occurred!')
+# logger.info('This is an info message in white color!')
+
+logging_config().logging().debug('This is a debug message in green color!')
--- cowsay.py
+++ cowsay.py
@@ -1,2 +1,3 @@
 import cowsay
-print(cowsay.get_output_string('trex', 'Hello (extinct) World'))
+
+print(cowsay.cow("Hi"))
