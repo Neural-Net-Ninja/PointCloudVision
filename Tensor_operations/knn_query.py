b67552c9459b18cc03792ab449c22f3fa68e336a
dhananjay.umalkar <dhananjay.umalkar@pointcloudtechnology.com>
2023-05-11 22:20:26 +0200

    added shape comments for knn

--- Tensor_operations/knn_query.py
+++ Tensor_operations/knn_query.py
@@ -2,3 +2,11 @@ import torch
 import pointops
 
 # pointops knn_query takes in (neighbours, coord, offset)
+
+# neighbours = 8
+# coord.size() = torch.Size([2, 1024, 3])
+# offset = torch.Size([2])
+# offset = tensor([1024, 2048])
+
+# returns indices, distances
+# indices = torch.Size([2, 8])
