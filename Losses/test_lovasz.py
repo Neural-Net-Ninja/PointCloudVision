2ae1b6aef1feff9a17bb58de872066b5e1c6bd03
dhananjay.umalkar <dhananjay.umalkar@pointcloudtechnology.com>
2024-04-18 17:04:21 +0200

    rearranged lovasz input tensors

--- Losses/test_lovasz.py
+++ Losses/test_lovasz.py
@@ -26,3 +26,31 @@ def test_all_true(self, reduction: str, expected_loss: np.ndarray):
     actual_loss = loss_module(prediction, target).numpy()
 
     np.testing.assert_allclose(expected_loss, actual_loss)
+    
+@parameterized.expand([
+        ("mean", np.array(0.0)),
+        ("sum", np.array(0.0)),
+    ])
+    def test_all_true_1(self, reduction: str, expected_loss: np.ndarray):
+        """
+        Tests that the Lovasz loss is zero for a perfect prediction.
+        """
+
+        prediction = torch.as_tensor(
+            [
+                [1, 0, 0],  # Predicted probabilities for sample 1
+                [0, 1, 0],  # Predicted probabilities for sample 2
+                [0, 0, 1]   # Predicted probabilities for sample 3
+            ],
+            dtype=torch.float
+        )
+
+        target = torch.as_tensor([0, 1, 2])  # True class labels
+
+        loss_module = LovaszLoss(per_point=True, reduction=reduction)
+
+        actual_loss = loss_module(prediction, target).numpy()
+
+        np.testing.assert_allclose(expected_loss, actual_loss)
+        
+# rewritting lovasz loss functino input tensors to make them less sensitive to ordering.
