3d9b88a95498ca0424f544ce93b05d1119d68618
dhananjay.umalkar <dhananjay.umalkar@pointcloudtechnology.com>
2024-05-30 23:45:03 +0200

    proposed a logger for color info

--- color_log.py
+++ color_log.py
@@ -1,72 +1,41 @@
+# logger_config.py
 import logging
 import colorlog
 
+def get_logger():
+    # create logger
+    logger = logging.getLogger(__name__)
 
-class logging_config:
+    # set log level
+    logger.setLevel(logging.DEBUG)
 
-    def logging(self):
-        # create logger
-        logger = logging.getLogger(__name__)
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
 
-        # set log level
-        logger.setLevel(logging.DEBUG)
+    # create formatter with color scheme
+    formatter = colorlog.ColoredFormatter(
+        '%(log_color)s%(levelname)s:%(message)s',
+        log_colors={
+            'DEBUG': 'green',
+            'INFO': 'white',
+            'WARNING': 'yellow',
+            'ERROR': 'red',
+            'CRITICAL': 'red,bg_white',
+        }
+    )
 
-        # create console handler and set level to debug
-        ch = logging.StreamHandler()
-        ch.setLevel(logging.DEBUG)
+    # add formatter to console handler
+    ch.setFormatter(formatter)
 
-        # create formatter with color scheme
-        formatter = colorlog.ColoredFormatter(
-            '%(log_color)s%(levelname)s:%(message)s',
-            log_colors={
-                'DEBUG': 'green',
-                'INFO': 'white',
-                'WARNING': 'yellow',
-                'ERROR': 'red',
-                'CRITICAL': 'red,bg_white',
-            }
-        )
+    # add console handler to logger
+    logger.addHandler(ch)
 
-        # add formatter to console handler
-        ch.setFormatter(formatter)
+    return logger
 
-        # add console handler to logger
-        logger.addHandler(ch)
+# some_other_file.py
+from logger_config import get_logger
 
-        return logger
-# # create logger
-# logger = logging.getLogger(__name__)
+logger = get_logger()
 
-# # set log level
-# logger.setLevel(logging.DEBUG)
-
-# # create console handler and set level to debug
-# ch = logging.StreamHandler()
-# ch.setLevel(logging.DEBUG)
-
-# # create formatter with color scheme
-# formatter = colorlog.ColoredFormatter(
-#     '%(log_color)s%(levelname)s:%(message)s',
-#     log_colors={
-#         'DEBUG': 'green',
-#         'INFO': 'white',
-#         'WARNING': 'yellow',
-#         'ERROR': 'red',
-#         'CRITICAL': 'red,bg_white',
-#     }
-# )
-
-# # add formatter to console handler
-# ch.setFormatter(formatter)
-
-# # add console handler to logger
-# logger.addHandler(ch)
-
-# # log a debug message
-# logger.debug('This is a debug message in green color!')
-# logger.error('An error occurred!')
-# logger.warning('Something might be wrong!')
-# logger.critical('A critical error occurred!')
-# logger.info('This is an info message in white color!')
-
-logging_config().logging().debug('This is a debug message in green color!')
+logger.debug('This is a debug message in green color!')
