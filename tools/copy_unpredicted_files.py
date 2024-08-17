88f9a958d6502476662cc9b8904e32147f41f7c3
dhananjay.umalkar <dhananjay.umalkar@pointcloudtechnology.com>
2024-08-17 23:00:16 +0200

    Refactor copy_unpredicted_files.py to improve file copying efficiency

--- tools/copy_unpredicted_files.py
+++ tools/copy_unpredicted_files.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 
+
 def get_file_names_without_extension(folder):
     return {os.path.splitext(file)[0] for file in os.listdir(folder)}
 
@@ -16,6 +17,10 @@ def copy_unpredicted_files(source_folder, predicted_folder, destination_folder):
     # Determine the base names of files that have not been predicted yet
     unpredicted_files = source_files - predicted_files
 
+    # Total number of files to copy
+    total_files = len(unpredicted_files)
+    copied_files = 0
+
     # Copy unpredicted files to the destination folder
     for file_name in os.listdir(source_folder):
         base_name = os.path.splitext(file_name)[0]
@@ -23,13 +28,29 @@ def copy_unpredicted_files(source_folder, predicted_folder, destination_folder):
             src_file = os.path.join(source_folder, file_name)
             dest_file = os.path.join(destination_folder, file_name)
             shutil.copy(src_file, dest_file)
-            print(f"Copied {file_name} to {destination_folder}")
+            copied_files += 1
+            # Calculate and print the percentage of completion
+            percentage_complete = (copied_files / total_files) * 100
+            print(f"Copying files: {percentage_complete:.2f}% complete", end='\r')
+
+    print("\nCopying complete.")
 
 
 # Define the paths to your folders
-folder_a = r'K:\Spie\Spie_files\paket_6\L513'
-folder_b = r'K:\Spie\Spie_files\PCT_spie_files\paket_6\L513'
-folder_c = r'K:\Spie\Spie_files\Spie_ML\02_output'
+all_files = r'K:\Spie\Spie_files\Spie_ML\01_preprocessed\paket_6_before_cut\L315'
+predicted_files = r'K:\Spie\Spie_files\Spie_ML\02_output\paket_6\L315'
+yet_to_be_predicted = r'K:\Spie\Spie_files\Spie_ML\01_preprocessed\delete'
+
+# Count the number of files in the all_files folder
+all_files_count = len([name for name in os.listdir(all_files) if os.path.isfile(os.path.join(all_files, name))])
+print(f"Number of files in all_files folder: {all_files_count}")
+
+# Count the number of files in the predicted_files folder
+predicted_files_count = len([name for name in os.listdir(predicted_files) if os.path.isfile(os.path.join(predicted_files, name))])
+print(f"Number of files in predicted_files folder: {predicted_files_count}")
+
+yet_to_be_predicted_count = all_files_count - predicted_files_count
+print(f"Number of files to be predicted: {yet_to_be_predicted_count}")
 
 # Copy the unpredicted files
-copy_unpredicted_files(folder_a, folder_b, folder_c)
+copy_unpredicted_files(all_files, predicted_files, yet_to_be_predicted)
