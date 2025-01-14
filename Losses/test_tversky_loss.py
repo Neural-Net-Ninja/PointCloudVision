34f0d7a56f972162787a8b5841a066084e9305b0
dhananjay.umalkar <dhananjay.umalkar@pointcloudtechnology.com>
2025-01-14 18:41:57 +0100

    add: enhance TverskyLoss tests with parameterized cases for various scenarios

--- Losses/test_tversky_loss.py
+++ Losses/test_tversky_loss.py
@@ -1,42 +1,121 @@
 import unittest
+from parameterized import parameterized
+import numpy as np
 import torch
-from Tversky_loss import TverskyLoss
+import torch.nn.functional as F
+
+from pcnn.losses import TverskyLoss
+
 
 class TestTverskyLoss(unittest.TestCase):
-    def setUp(self):
-        self.inputs = torch.tensor([[[[0.7, 0.2], [0.4, 0.6]]]], dtype=torch.float32)
-        self.targets = torch.tensor([[[[1, 0], [1, 1]]]], dtype=torch.long)
-        self.epsilon = 1e-5  # Small value to account for floating-point arithmetic errors
+    @parameterized.expand([
+        ("mean", 0.5, 0.5, 1.0, np.array(0.0)),
+        ("sum", 0.5, 0.5, 1.0, np.array(0.0)),
+        ("none", 0.5, 0.5, 1.0, np.array([0.0, 0.0, 0.0])),
+    ])
+    def test_all_true(self, reduction: str, alpha: float, beta: float, epsilon: float, expected_loss: np.ndarray):
+        """
+        Tests that the Tversky loss is computed correctly when all predictions are correct.
+        """
+        prediction = torch.as_tensor(
+            [
+                [0, 0, 1],
+                [1, 0, 0],
+                [0, 1, 0]
+            ],
+            dtype=torch.float
+        )
 
-    def test_initialization(self):
-        loss = TverskyLoss()
-        self.assertEqual(loss.alpha, 0.5)
-        self.assertEqual(loss.beta, 0.5)
-        self.assertEqual(loss.smooth, 1.0)
-        self.assertEqual(loss.reduction, 'mean')
-        self.assertIsNone(loss.class_weights)
-        self.assertFalse(loss.dynamic_focus)
+        target = torch.as_tensor([2, 0, 1])
 
-    def test_forward_pass(self):
-        loss = TverskyLoss()
-        result = loss(self.inputs, self.targets)
-        self.assertTrue(torch.is_tensor(result))
-        self.assertGreater(result.item(), 0)
-        
-    def test_class_weights(self):
-        class_weights = torch.tensor([0.5, 2.0])
-        loss = TverskyLoss(class_weights=class_weights)
-        result_with_weights = loss(self.inputs, self.targets)
-        result_without_weights = TverskyLoss()(self.inputs, self.targets)
-        self.assertNotEqual(result_with_weights.item(), result_without_weights.item())
+        loss_module = TverskyLoss(apply_softmax=False, alpha=alpha, beta=beta, epsilon=epsilon, reduction=reduction)
 
-    def test_dynamic_focus(self):
-        loss = TverskyLoss(dynamic_focus=True)
-        initial_alpha = loss.alpha
-        initial_beta = loss.beta
-        loss(self.inputs, self.targets)  # Trigger dynamic focus adjustment
-        self.assertNotEqual(initial_alpha, loss.alpha)
-        self.assertNotEqual(initial_beta, loss.beta)
+        actual_loss = loss_module(prediction, target).numpy()
+
+        np.testing.assert_equal(expected_loss, actual_loss)
+
+    @parameterized.expand([
+        ("mean", 0.5, 0.5, 1.0, np.array(1.0)),
+        ("sum", 0.5, 0.5, 1.0, np.array(3.0)),
+        ("none", 0.5, 0.5, 1.0, np.array([1.0, 1.0, 1.0])),
+    ])
+    def test_all_false(self, reduction: str, alpha: float, beta: float, epsilon: float, expected_loss: np.ndarray):
+        """
+        Tests that the Tversky loss is computed correctly when all predictions are wrong.
+        """
+        prediction = torch.as_tensor(
+            [
+                [1, 0, 0],
+                [0, 0, 1],
+                [0, 1, 0]
+            ],
+            dtype=torch.float
+        )
+
+        target = torch.as_tensor([1, 0, 2])
+
+        loss_module = TverskyLoss(apply_softmax=False, alpha=alpha, beta=beta, epsilon=epsilon, reduction=reduction)
+
+        actual_loss = loss_module(prediction, target).numpy()
+
+        np.testing.assert_allclose(expected_loss, actual_loss)
+
+    @parameterized.expand([
+        ("mean", 0.5, 0.5, 1.0, np.array([0.25, 0.2, 0.1]).mean()),
+    ])
+    def test_probabilistic_predictions(self, reduction: str,
+                                       alpha: float,
+                                       beta: float,
+                                       epsilon: float,
+                                       expected_loss: np.ndarray):
+        """
+        Tests that the Tversky loss is computed correctly when the predictions are probabilities between zero and one.
+        """
+        prediction = torch.as_tensor(
+            [
+                [0.5, 0.25, 0.25],
+                [0.8, 0.1, 0.1],
+                [0.6, 0.3, 0.1]
+            ],
+            dtype=torch.float
+        )
+
+        target = torch.as_tensor([1, 0, 2])
+
+        loss_module = TverskyLoss(apply_softmax=False, alpha=alpha, beta=beta, epsilon=epsilon, reduction=reduction)
+
+        actual_loss = loss_module(prediction, target).numpy()
+
+        np.testing.assert_allclose(expected_loss, actual_loss, atol=1e-07)
+
+    @parameterized.expand([
+        ("mean", 0.5, 0.5, 1.0, np.array([0.1, 0.2, 0.7]), np.array([0.25, 0.2, 0.1]).mean()),
+    ])
+    def test_weighted_loss(self,
+                           reduction: str,
+                           alpha: float,
+                           beta: float,
+                           epsilon: float,
+                           weight: np.ndarray,
+                           expected_loss: np.ndarray):
+        """Tests that the Tversky loss is computed correctly when class weights are provided."""
+        prediction = torch.as_tensor(
+            [
+                [0.5, 0.25, 0.25],
+                [0.8, 0.1, 0.1],
+                [0.6, 0.3, 0.1]
+            ],
+            dtype=torch.float
+        )
+
+        target = torch.as_tensor([1, 0, 2])
+
+        loss_module = TverskyLoss(apply_softmax=False, alpha=alpha, beta=beta, epsilon=epsilon, reduction=reduction,
+                                  weight=torch.from_numpy(weight).float())
+
+        actual_loss = loss_module(prediction, target).numpy()
+
+        np.testing.assert_allclose(expected_loss, actual_loss, atol=1e-07)
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(exit=False)
