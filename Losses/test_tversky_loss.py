1346b1b624fd301db5bf593e4bd33141612e98b1
dhananjay.umalkar <dhananjay.umalkar@pointcloudtechnology.com>
2025-01-20 20:56:30 +0100

    refactor: enhance TverskyLoss tests with additional scenarios and improve clarity

--- Losses/test_tversky_loss.py
+++ Losses/test_tversky_loss.py
@@ -2,23 +2,45 @@ import unittest
 from parameterized import parameterized
 import numpy as np
 import torch
-import torch.nn.functional as F
 
 from pcnn.losses import TverskyLoss
 
 
 class TestTverskyLoss(unittest.TestCase):
     @parameterized.expand([
-        ("mean", 0.5, 0.5, 1.0, np.array([0.25, 0.2, 0.1]).mean()),
+        ("mean", 0.5, 0.5, 1.0, np.array(0.0)),
+        ("sum", 0.5, 0.5, 1.0, np.array(0.0)),
     ])
-    def test_probabilistic_predictions(self, reduction: str,
-                                       alpha: float,
-                                       beta: float,
-                                       epsilon: float,
-                                       expected_loss: np.ndarray):
+    def test_all_true(self, reduction: str, alpha: float, beta: float, epsilon: float, expected_loss: np.ndarray):
         """
-        Tests that the Tversky loss is computed correctly when the predictions are probabilities between zero and one.
+        Tests that the Tversky loss is computed correctly when all predictions are correct.
         """
+        prediction = torch.as_tensor(
+            [
+                [0, 0, 1],
+                [1, 0, 0],
+                [0, 1, 0]
+            ],
+            dtype=torch.float
+        )
+
+        target = torch.as_tensor([2, 0, 1])
+
+        loss_module = TverskyLoss(apply_softmax=False, alpha=alpha, beta=beta, epsilon=epsilon, reduction=reduction)
+
+        actual_loss = loss_module(prediction, target).numpy()
+
+        np.testing.assert_equal(expected_loss, actual_loss)
+
+    @parameterized.expand([
+        ("mean", np.array([0.1, 0.2, 0.7]), np.array([
+            0.1 * (1 - (0.8 + 1) / (0.8 + 0.3 * 0.5 + 0.7 * 0.6 + 1)),
+            0.2 * (1 - (0.25 + 1) / (0.25 + 0.3 * 1 + 0.7 * 0.1 + 1)),
+            0.7 * (1 - (0.1 + 1) / (0.1 + 0.3 * 0.25 + 0.7 * 1 + 1))
+        ]).mean())
+    ])
+    def test_weighted_loss(self, reduction: str, weight: np.ndarray, expected_loss: np.ndarray):
+        """Tests that the Tversky loss is computed correctly when class weights are provided."""
         prediction = torch.as_tensor(
             [
                 [0.5, 0.25, 0.25],
@@ -30,7 +52,8 @@ class TestTverskyLoss(unittest.TestCase):
 
         target = torch.as_tensor([1, 0, 2])
 
-        loss_module = TverskyLoss(apply_softmax=False, alpha=alpha, beta=beta, epsilon=epsilon, reduction=reduction)
+        loss_module = TverskyLoss(apply_softmax=False, alpha=0.3, beta=0.7, epsilon=1, reduction=reduction,
+                                  weight=torch.from_numpy(weight).float())
 
         actual_loss = loss_module(prediction, target).numpy()
 
